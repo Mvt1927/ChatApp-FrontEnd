b43ae16173b83205d04b7817124653edf43d4cef
Mvt1927 <viettheog4@gmail.com>
2022-09-19 20:35:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import IFriend from "./components/ifriend";
+import "./App.css";
+
+
+function App() {
+  var testObj = {
+    src_AvatarFriend:'https://scontent.fdad3-3.fna.fbcdn.net/v/t39.30808-1/274667595_105812578700442_338875092787591085_n.jpg?stp=dst-jpg_p100x100&_nc_cat=111&ccb=1-7&_nc_sid=7206a8&_nc_ohc=cBP3MkrBkPgAX9qG4uG&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.fdad3-3.fna&oh=00_AT8cbdCEnkhRh2axzNXmwhYO3YrJp2SvlZprALU1jD47dg&oe=632DD10D',
+    t_nameFriend: 'Quốc Việt',
+    t_lastMessFriend: 'Việt đã gọi cho bạn.' ,
+    time_timeLastMessFriend: 'Mon Sep 10 2021 21:24:40 GMT+0700 (Giờ Đông Dương)',
+  }
+  var checkScroll = (e)=>{
+    e.preventDefault();
+    var element = document.getElementById("under_line")
+    if (e.currentTarget.scrollTop > 5) {
+      element.className  = "border-b";
+    } else {
+      element.className = "";
+    }
+  }
+  return (
+    <div className="App flex flex-row">
+      <div className="left border-black flex flex-col flex-shrink-0 flex-grow-0">
+        <div className="user-top-left flex flex-col px-4 py-2.5">
+          <div className="user flex flex-row pad">
+            <div className="avatar-left w-3/12 flex">
+              <div className="w-9 h-9 hover:bg-[#00000020] hover:opacity-100 rounded-full cursor-pointer">
+                <img
+                  className="w-9 h-9 rounded-full hover:mix-blend-overlay hover:bg-black"
+                  src="./public/avatar.jpg"
+                  alt="avatar"
+                />
+              </div>
+            </div>
+            <div className="app-name-mid w-6/12 self-center">
+              <p className="text-center text-base">Chat</p>
+            </div>
+            <div className="fun-btn-right w-3/12 text-center flex flex-row justify-center">
+              <div className="meeting-button-left w-9 h-9 flex items-center self-center hover:rounded-full hover:bg-f2f2f2 cursor-pointer">
+                <div className="meeting-icon">
+                  <i className="fa-light fa-video-plus flex items-center justify-center text-lg"></i>
+                </div>
+              </div>
+              <div className="new-button-right w-9 h-9 flex items-center self-center hover:rounded-full hover:bg-f2f2f2 cursor-pointer">
+                <div className="new-icon">
+                  <i className="icon bi bi-pencil-square flex items-center justify-center text-lg"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="search-mid h-14 px-4 pb-3 pt-1.5">
+          <div className="search-box flex flex-row rounded-full bg-f2f2f2 h-full">
+            <div className="search-icon-left flex items-center justify-end w-1/12">
+              <i className="fa-light fa-magnifying-glass text-base"></i>
+            </div>
+            <input
+              className="search-input-right px-1.5 pt-2.252 pb-1.752 text-sm font-light w-full rounded-r-full bg-f2f2f2 placeholder:text-sm placeholder:text-7C676B focus:outline-none"
+              type={"search"}
+              placeholder="Tìm kiếm trên Messenger"
+              aria-label="Tìm kiếm trên Messenger"
+            />
+          </div>
+        </div>
+        <div id="under_line"></div>
+        <div className="list-friend-bottom overflow:hidden h-10/12">
+          <div onScroll={checkScroll} className="overflow-scroll px-1.5 max-h-full"  >
+            <IFriend className="selection" obj={testObj}/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+            <IFriend/>
+          </div>
+        </div>
+      </div>
+      <div className="right border-black">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ifriend.jsx
@@ -0,0 +1,69 @@
+import { Component, useState } from "react";
+class IFriend extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var srcAvatarFriend = './public/avatar.jpg'
+    var nameFriend = 'Dm moi'
+    var lastestMessageFriend = "Dm thang moi"
+    var timeLastestMessageFriend ="Mon Sep 19 2022 10:11:40 GMT+0700 (Giờ Đông Dương)"
+    if (this.props.obj!=null) {
+      srcAvatarFriend = this.props.obj.src_AvatarFriend
+      nameFriend = this.props.obj.t_nameFriend
+      lastestMessageFriend = this.props.obj.t_lastMessFriend
+      timeLastestMessageFriend = this.props.obj.time_timeLastMessFriend
+    }
+    if (timeLastestMessageFriend!=null&& new Date(timeLastestMessageFriend)!='Invalid Date') {
+      var currentDate = new Date();
+      var lastDate = new Date(timeLastestMessageFriend);
+      var text_year = ' year'
+      var text_week = ' week'
+      var text_day = ' day'
+      var text_hour =' hour'
+      var text_minute =' minute'
+      var text_second = ' second'
+      var timeAway = 
+      ((currentDate - lastDate)/1000)>60 /**s */
+      ?((currentDate - lastDate)/1000/60)>60 /** m */
+      ?((currentDate - lastDate)/1000/60/60)>24 /** h */
+      ?((currentDate - lastDate)/1000/60/60/24)>7 /** d */
+      ?((currentDate - lastDate)/1000/60/60/24/7)>53 /**w */
+      ?parseInt((currentDate - lastDate)/1000/60/60/24/365)+""+text_year 
+      :parseInt((currentDate - lastDate)/1000/60/60/24/7)+""+text_week
+      :parseInt((currentDate - lastDate)/1000/60/60/24)+""+text_day
+      :parseInt((currentDate - lastDate)/1000/60/60)+""+text_hour
+      :parseInt((currentDate - lastDate)/1000/60)+""+text_minute
+      :parseInt((currentDate - lastDate)/1000)+""+text_second
+    }
+    return (
+      <div className={"item-friend-container p-2.5 h-17 box-border hover:bg-f2f2f2 hover:rounded-lg cursor-pointer first-line:"+ this.props.className}>
+        <div className="ite./public/avatar.jpgm-friend w-full h-full flex flex-row">
+          <div className="friend-avatar w-1/5 pr-2.5 ">
+            <img
+              className="w-12 h-12 rounded-full box-border"
+              src={srcAvatarFriend}
+              alt="avatar"
+            />
+          </div>
+          <div className="friend-info w-4/5 flex flex-col">
+            <span className="name-friend text text-base">{nameFriend}</span>
+            <div className="h-2"></div>
+            <div className="lastest-message flex flex-row">
+              <span className="short-text-latest-message max-w-fit w-4/6 text-xs font-light whitespace-nowrap truncate">
+                {lastestMessageFriend}
+              </span>
+              <p className="text-xs">&nbsp;</p>
+              <p className="text-xs"> · </p>
+              <p className="text-xs">&nbsp;</p>
+              <span className="time-receive-latest-message w-1/6 text-xs font-light whitespace-nowrap">
+                {timeAway}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default IFriend;
